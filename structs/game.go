71c741752c8849823ea6e48d9f2c9eb7f7f7d0b6
Jake Rosch <JakeRosch@letu.edu>
2022-11-07 22:44:20 -0600

    added basic game structs

--- structs/game.go
+++ structs/game.go
@@ -1,8 +1,19 @@
 package structs
 
+import "github.com/gorilla/websocket"
+
+type PlayerRole int
+
+const (
+	RoleHost PlayerRole = iota
+	RolePlayer
+)
+
 type GamePlayer struct {
 	Player Player
+	Conn *websocket.Conn
 	Points int
+	Role PlayerRole
 }
 
 type TurnState struct {

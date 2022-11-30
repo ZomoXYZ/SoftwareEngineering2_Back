dbbef7854ceb1a22e7e9ba4170049eaf88de8eec
Jake Rosch <JakeRosch@letu.edu>
2022-11-30 11:14:33 -0600

    meta name data

--- endpoints/meta.go
+++ endpoints/meta.go
@@ -2,25 +2,39 @@ package endpoints
 
 import (
 	"edu/letu/wan/database"
+	"encoding/json"
+	"io/ioutil"
 
 	"github.com/gin-gonic/gin"
 )
+
+type MetaNamesJSON struct {
+	Adjectives map[int]string `json:"adjectives"`
+	Nouns map[int]string `json:"nouns"`
+}
+
+type MetaAvatarsJSON struct {
+	Avatars map[int]string `json:"avatars"`
+}
 
 func MetaNames(c *gin.Context) {
 	if !database.IsAuthorized(c) {
 		return
 	}
-	
-	// TODO make it a map instead of a list
-	// {"names": {"0": "name", "1": "name", "2": "name"}}
-	c.JSON(200, gin.H{
-		"names": []string{
-			"John",
-			"Paul",
-			"George",
-			"Ringo",
-		},
-	})
+
+	content, err := ioutil.ReadFile("./resources/names.json")
+    if err != nil {
+        c.AbortWithStatus(500)
+    }
+ 
+	// validate json before sending it
+    var payload MetaNamesJSON
+    err = json.Unmarshal(content, &payload)
+    if err != nil {
+        c.AbortWithStatus(500)
+    }
+
+	c.JSON(200, payload)
 }
 
 func MetaPictures(c *gin.Context) {

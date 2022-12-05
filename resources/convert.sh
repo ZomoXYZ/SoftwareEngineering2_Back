320322d4e8a57d27589ebc4663c8139d2fdd9ec5
Jake Rosch <JakeRosch@letu.edu>
2022-12-04 18:12:06 -0600

    new images

--- resources/convert.sh
+++ resources/convert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for path in ./avatars_originals/*.png; do
+for path in ./avatars_originals/*; do
     filename=${path##*/}
     name=${filename%.*}
     convert \( -size 256x256 xc:#ffffff -colorspace sRGB \) \( ./avatars_originals/$filename -resize 256x256 -colorspace sRGB \) -gravity center -compose over -composite -type TrueColor ./avatars/$name.png
